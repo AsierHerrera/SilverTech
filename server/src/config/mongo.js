43e53fa38d2b3f4ace802448785cec6db282632a
asier <asierherrera97@gmail.com>
2024-07-01 15:23:12 +0200

    Login funcional desde el front

--- server/src/config/mongo.js
+++ server/src/config/mongo.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_PORT = process.env.DB_PORT || "27017";
+const DB_PORT = "27017";
 const DB_NAME = process.env.DB_NAME || "Desafio";
 const DB_USER = process.env.DB_USER || "Asier";
 const DB_PASSWORD = process.env.DB_PASSWORD || "1234";
