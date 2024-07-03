655eabf2403fc214e4065505db92f734e2d06dd4
cristobendito <flopybbb@gmail.com>
2024-07-03 18:11:48 +0200

    nuevo stilo

--- front/src/pages/UserPanel.jsx
+++ front/src/pages/UserPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProfilePicUpload from '../../src/pages/userProfile/ProfilePicUpload';
-import "../pages/userProfile/UserProfile.css"
+import "../pages/userProfile/UserProfile.css";
 
 const UserPanel = () => {
     const [user, setUser] = useState({});
@@ -65,143 +65,134 @@ const UserPanel = () => {
         alert('¡Datos de la empresa guardados correctamente!');
     };
 
-    const formGroupStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '10px'
-    };
-
-    const labelStyle = {
-        marginRight: '10px',
-        minWidth: '150px'
-    };
-
     return (
         <div className='user-profile'>
-            <h1>Perfil</h1>
-            {profilePic && <img src={profilePic} alt='Profile' style={{ width: '150px', height: '150px', borderRadius: '50%', alignItems: 'center' }} />}
+            <div className='form-container'>
+                <h1>Formulario de la empresa</h1>
+                <div className='profile-header'>
+                    {profilePic && <img src={profilePic} alt='Profile' className='profile-pic' />}
+                    <h2>{user.username}</h2>
+                    <p>@{user.username}</p>
+                </div>
 
-            <div className='profile-header'>
-                <h2>{user.username}</h2>
-                <p>@{user.username}</p>
-            </div>
+                <div className='profile-details'>
+                    <form onSubmit={handleSubmit}>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Nombre de la empresa</label>
+                            <input
+                                type="text"
+                                name="nombreEmpresa"
+                                placeholder="Nombre de la empresa"
+                                value={formData.nombreEmpresa}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Número de CIF</label>
+                            <input
+                                type="text"
+                                name="cif"
+                                placeholder="Número de CIF"
+                                value={formData.cif}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Dirección</label>
+                            <input
+                                type="text"
+                                name="direccion"
+                                placeholder="Dirección"
+                                value={formData.direccion}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Código postal</label>
+                            <input
+                                type="text"
+                                name="codigoPostal"
+                                placeholder="Código postal"
+                                value={formData.codigoPostal}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Sitio Web</label>
+                            <input
+                                type="text"
+                                name="sitioWeb"
+                                placeholder="Sitio Web"
+                                value={formData.sitioWeb}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Teléfono</label>
+                            <input
+                                type="text"
+                                name="telefono"
+                                placeholder="Teléfono"
+                                value={formData.telefono}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Sector</label>
+                            <input
+                                type="text"
+                                name="sector"
+                                placeholder="Sector"
+                                value={formData.sector}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Descripción</label>
+                            <textarea
+                                name="descripcion"
+                                placeholder="Descripción"
+                                value={formData.descripcion}
+                                onChange={handleChange}
+                                rows="4"
+                            />
+                        </div>
+                        <div className="formGroupStyle">
+                            <label className="labelStyle">Subir imagen</label>
+                            <ProfilePicUpload setProfilePic={setProfilePic} />
+                        </div>
+                    </form>
+                </div>
 
-            <div className='profile-details'>
-                <form onSubmit={handleSubmit}>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Nombre de la empresa:</strong></label>
+                <div className='profile-actions'>
+                    <form onSubmit={handleNameChange}>
+                        <h3>Cambiar Nombre de Usuario</h3>
                         <input
-                            type="text"
-                            name="nombreEmpresa"
-                            placeholder="Nombre de la empresa"
-                            value={formData.nombreEmpresa}
-                            onChange={handleChange}
+                            type='text'
+                            value={name}
+                            placeholder="Cambiar Nombre"
+                            onChange={(e) => setName(e.target.value)}
                         />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Número de CIF:</strong></label>
-                        <input
-                            type="text"
-                            name="cif"
-                            placeholder="Número de CIF"
-                            value={formData.cif}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Dirección:</strong></label>
-                        <input
-                            type="text"
-                            name="direccion"
-                            placeholder="Dirección"
-                            value={formData.direccion}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Código postal:</strong></label>
-                        <input
-                            type="text"
-                            name="codigoPostal"
-                            placeholder="Código postal"
-                            value={formData.codigoPostal}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Sitio Web:</strong></label>
-                        <input
-                            type="text"
-                            name="sitioWeb"
-                            placeholder="Sitio Web"
-                            value={formData.sitioWeb}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Teléfono:</strong></label>
-                        <input
-                            type="text"
-                            name="telefono"
-                            placeholder="Teléfono"
-                            value={formData.telefono}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Sector:</strong></label>
-                        <input
-                            type="text"
-                            name="sector"
-                            placeholder="Sector"
-                            value={formData.sector}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div style={formGroupStyle}>
-                        <label style={labelStyle}><strong>Descripción:</strong></label>
-                        <input
-                            type="text"
-                            name="descripcion"
-                            placeholder="Descripción"
-                            value={formData.descripcion}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </form>
-            </div>
-            <ProfilePicUpload setProfilePic={setProfilePic} />
+                        <button type='submit'>Actualizar Nombre</button>
+                    </form>
 
-            <div className='profile-actions'>
-                <form onSubmit={handleNameChange}>
-                    <h3>Cambiar Nombre de Usuario</h3>
-                    <input
-                        type='text'
-                        value={name}
-                        placeholder="Cambiar Nombre"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <button type='submit'>Actualizar Nombre</button>
-                </form>
-
-                <form onSubmit={handlePasswordChange}>
-                    <h4>Cambiar Contraseña</h4>
-                    <input
-                        type='password'
-                        placeholder='Contraseña Actual'
-                        value={currentPassword}
-                        onChange={(e) => setCurrentPassword(e.target.value)}
-                    />
-                    <input
-                        type='password'
-                        placeholder='Nueva Contraseña'
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                    <button type='submit'>Actualizar Contraseña</button>
-                </form>
-
-                <button type="submit" onClick={handleSubmit}>Guardar</button>
+                    <form onSubmit={handlePasswordChange}>
+                        <h4>Cambiar Contraseña</h4>
+                        <input
+                            type='password'
+                            placeholder='Contraseña Actual'
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                        />
+                        <input
+                            type='password'
+                            placeholder='Nueva Contraseña'
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                        <button type='submit'>Actualizar Contraseña</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
