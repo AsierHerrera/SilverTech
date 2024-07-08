6a2d0b09a4eadb21f3329e46bf8faaec0a16a370
Luis Padua <luispadua2203@gmail.com>
2024-07-08 10:38:27 +0200

    actualizando

--- front/src/componentes/recurso/CreateRecurso.jsx
+++ front/src/componentes/recurso/CreateRecurso.jsx
@@ -21,26 +21,31 @@ const CreateRecurso = ({onCreate})=>{
         window.location.reload()
     }
     return (
-        <form action="" className="create-recurso" onSubmit={handleSubmit}>
-            <label htmlFor="img" >Imagen</label>
-            <input type="file" name="img"/>
-            <label htmlFor="name" >Nombre</label>
-            <input type="text" name="name"/>
-            <label htmlFor="description" >Description</label>
-            <textarea name="description"></textarea>
-            <label htmlFor="day" >Dia</label>
-            <input type="text" name="day"/>
-            <label htmlFor="time" >Hora</label>
-            <input type="text" name="time"/>
-            <label htmlFor="place" >Lugar</label>
-            <input type="text" name="place"/>
-            <label htmlFor="tickes" >Asistentes</label>
-            <input type="text" name="tickes"/>
-            <label htmlFor="price" >Precio</label>
-            <input type="text" name="price"/>
+        <>
+        {/*
+            <form action="" className="create-recurso" onSubmit={handleSubmit}>
+                <label htmlFor="img" >Imagen</label>
+                <input type="file" name="img"/>
+                <label htmlFor="name" >Nombre</label>
+                <input type="text" name="name"/>
+               <label htmlFor="description" >Description</label>
+                <textarea name="description"></textarea>
+                <label htmlFor="day" >Dia</label>
+                <input type="text" name="day"/>
+                <label htmlFor="time" >Hora</label>
+                <input type="text" name="time"/>
+                <label htmlFor="place" >Lugar</label>
+                <input type="text" name="place"/>
+                <label htmlFor="tickes" >Asistentes</label>
+                <input type="text" name="tickes"/>
+                <label htmlFor="price" >Precio</label>
+                <input type="text" name="price"/>
 
-            <button type="submit" className="btn-create-recurso">Crear</button>
-        </form>
+                <button type="submit" className="btn-create-recurso">Crear</button>
+            </form>
+        */}
+        
+        </>
     )
 }
 export default CreateRecurso;
